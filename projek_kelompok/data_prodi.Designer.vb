c42674d8008a6d0b8033a2a69aa34f2776b19e68
muhammadzakyrahman20-rgb <muhammadzakyrahman20@gmail.com>
2025-11-12 15:08:35 +0700

    perubahan button

--- projek_kelompok/data_prodi.Designer.vb
+++ projek_kelompok/data_prodi.Designer.vb
@@ -34,33 +34,38 @@ Partial Class data_prodi
         '
         'Panel1
         '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(80, Byte), Integer))
         Me.Panel1.Controls.Add(Me.btnEdit)
         Me.Panel1.Controls.Add(Me.txtNamaProdi)
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.btnSimpan)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(5)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1633, 790)
+        Me.Panel1.Size = New System.Drawing.Size(1107, 793)
         Me.Panel1.TabIndex = 8
         '
         'btnEdit
         '
-        Me.btnEdit.Location = New System.Drawing.Point(1128, 453)
-        Me.btnEdit.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnEdit.BackColor = System.Drawing.Color.Blue
+        Me.btnEdit.Font = New System.Drawing.Font("Cooper Black", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.ForeColor = System.Drawing.Color.White
+        Me.btnEdit.Location = New System.Drawing.Point(566, 254)
+        Me.btnEdit.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.btnEdit.Name = "btnEdit"
-        Me.btnEdit.Size = New System.Drawing.Size(274, 98)
+        Me.btnEdit.Size = New System.Drawing.Size(137, 51)
         Me.btnEdit.TabIndex = 29
         Me.btnEdit.Text = "Edit"
-        Me.btnEdit.UseVisualStyleBackColor = True
+        Me.btnEdit.UseVisualStyleBackColor = False
         '
         'txtNamaProdi
         '
-        Me.txtNamaProdi.Location = New System.Drawing.Point(814, 312)
-        Me.txtNamaProdi.Margin = New System.Windows.Forms.Padding(5)
+        Me.txtNamaProdi.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.txtNamaProdi.Location = New System.Drawing.Point(407, 186)
+        Me.txtNamaProdi.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.txtNamaProdi.Name = "txtNamaProdi"
-        Me.txtNamaProdi.Size = New System.Drawing.Size(588, 38)
+        Me.txtNamaProdi.Size = New System.Drawing.Size(296, 22)
         Me.txtNamaProdi.TabIndex = 20
         '
         'Label2
@@ -68,43 +73,49 @@ Partial Class data_prodi
         Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(576, 318)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label2.Font = New System.Drawing.Font("Cooper Black", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(292, 188)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(163, 32)
+        Me.Label2.Size = New System.Drawing.Size(111, 20)
         Me.Label2.TabIndex = 19
         Me.Label2.Text = "Nama Prodi"
         '
         'btnSimpan
         '
-        Me.btnSimpan.Location = New System.Drawing.Point(582, 453)
-        Me.btnSimpan.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnSimpan.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnSimpan.Font = New System.Drawing.Font("Cooper Black", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSimpan.ForeColor = System.Drawing.Color.White
+        Me.btnSimpan.Location = New System.Drawing.Point(382, 254)
+        Me.btnSimpan.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.btnSimpan.Name = "btnSimpan"
-        Me.btnSimpan.Size = New System.Drawing.Size(276, 99)
+        Me.btnSimpan.Size = New System.Drawing.Size(138, 51)
         Me.btnSimpan.TabIndex = 18
         Me.btnSimpan.Text = "Simpan"
-        Me.btnSimpan.UseVisualStyleBackColor = True
+        Me.btnSimpan.UseVisualStyleBackColor = False
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.DataGridView1.Location = New System.Drawing.Point(0, 790)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(5)
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 793)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 62
         Me.DataGridView1.RowTemplate.Height = 28
-        Me.DataGridView1.Size = New System.Drawing.Size(1633, 260)
+        Me.DataGridView1.Size = New System.Drawing.Size(1107, 134)
         Me.DataGridView1.TabIndex = 7
         '
         'data_prodi
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.DataGridView1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "data_prodi"
-        Me.Size = New System.Drawing.Size(1633, 1050)
+        Me.Size = New System.Drawing.Size(1107, 927)
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
